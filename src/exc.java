9501dbca4f6a7feab847b95dd08740025bbf0d23
yourUsername <yourEmail@domain.com>
2020-02-11 19:23:11 -0500

    ff

--- src/exc.java
+++ src/exc.java
@@ -17,6 +17,8 @@ String gee="";
      zork.put("no",0 );
      zork.put("alright",25);
      zork.put( "", 3 );
+     zork.put( "oky", 666 );
+     zork.put( "yess", 1500 );
 
      System.out.println( "user:"+ "Do you gonna offer for me a room?" );
      System.out.println( "zork:" + zork.put("yes", 1 ));
@@ -26,14 +28,33 @@ String gee="";
      System.out.println( "zork:" + zork.put( "",3 )+ ": do you have any other question?");
      System.out.println( "user:" + ":"+ input.nextLine() );
      // String x = input.nextLine();
-     System.out.println("Zork:" + "B-600" );
+     System.out.println("Zork:" + zork.put(  "", 254) );
 
      // for inhance loop
 
   //   for (Integer key : map.values()) {
   //      System.out.println("Key = " + key);
   //  }
+     System.out.println( "user" + "How much you gonna charge me per room?");
+     System.out.println( "Zork" + zork.put( "oky", 666 ));
+     System.out.println( "user" + "How much you charge me for all room");
+     System.out.println( "user"+ zork.put( "yess", 1500 ) );
 
+     // for inhance loop
+    // for ( String X : zork){
+    //     System.out.println( ""+ zork );
+    // }
+
+
+
+          int y = 0;
+     for(y=0; y < 3;y++){
+     System.out.println( "" + zork );
+
+
+
+
+     }
 
 }
 
